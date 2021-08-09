b5d1362e0805ab6aebdb0dfff5a976ab3ee67989
abhi277singh <277abhisheksingh@gmail.com>
2021-08-09 20:40:23 +0530

    initial commit

---
+++ models/campground.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const campgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    location: String,
+    description: String
+})
+module.exports = mongoose.model("Campground", campgroundSchema)
