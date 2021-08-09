0b381b1bb8f83d02d9156f553b2871d9409d5eff
abhi277singh <277abhisheksingh@gmail.com>
2021-08-09 22:54:55 +0530

    Index file

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Campground = require("../models/campground");
+const cities = require("./cities")
+const { places, descriptors } = require("./seedHelpers")
+
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"))
+db.once("open", () => {
+    console.log("Database Connected")
+})
+const sample = (array => array[Math.floor(Math.random() * array.length)])
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i <= 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
+        const camp = new Campground({
+
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dignissimos repellat ullam voluptas cum tempora velit neque quam quod cupiditate. Mollitia officiis sapiente tempore rem minima! Quia incidunt aspernatur sunt.",
+            location: `${cities[random1000].state}, ${cities[random1000].city}`,
+            price
+        })
+        await camp.save();
+    }
+
+}
+seedDB().then(() => {
+    mongoose.connection.close()
+})
